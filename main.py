1952642e349f9a79ad599850c21550968db3ab53
pavel <psgaiduk@gmail.com>
2022-02-27 20:42:30 +0300

    added products in buttons

--- main.py
+++ main.py
@@ -8,23 +8,37 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Filters, Updater
 from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler
 from logger_handler import BotHandler
+from functools import partial
 
 
-_database = None
 logger = logging.getLogger('app_logger')
 
 
-def start(bot, update):
+def get_token(moltin_client_id, moltin_client_secret):
+    url = 'https://api.moltin.com/oauth/access_token'
+    data = {'client_id': moltin_client_id, 'client_secret': moltin_client_secret, 'grant_type': 'client_credentials'}
+    data_for_token = post(url=url, data=data).json()
+    return data_for_token['access_token']
+
+
+def get_products(token):
+    url = 'https://api.moltin.com/v2/products'
+    header = {'authorization': f'Bearer {token}', 'content-type': 'application/json'}
+    return get(url, headers=header).json()['data']
+
+
+def start(_, update, moltin_client_id, moltin_client_secret):
     """
     Хэндлер для состояния START.
 
     Бот отвечает пользователю фразой "Привет!" и переводит его в состояние ECHO.
     Теперь в ответ на его команды будет запускаеться хэндлер echo.
     """
-    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
-                 InlineKeyboardButton("Option 2", callback_data='2')],
 
-                [InlineKeyboardButton("Option 3", callback_data='3')]]
+    token = get_token(moltin_client_id, moltin_client_secret)
+    products = get_products(token)
+
+    keyboard = [[InlineKeyboardButton(product["name"], callback_data=product['id'])] for product in products]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
@@ -52,7 +66,7 @@ def button(bot, update):
                           message_id=query.message.message_id)
 
 
-def handle_users_reply(bot, update):
+def handle_users_reply(bot, update, moltin_client_id, moltin_client_secret, db):
     """
     Функция, которая запускается при любом сообщении от пользователя и решает как его обработать.
     Эта функция запускается в ответ на эти действия пользователя:
@@ -65,7 +79,6 @@ def handle_users_reply(bot, update):
     поэтому по этой фразе выставляется стартовое состояние.
     Если пользователь захочет начать общение с ботом заново, он также может воспользоваться этой командой.
     """
-    db = get_database_connection()
     if update.message:
         user_reply = update.message.text
         chat_id = update.message.chat_id
@@ -79,8 +92,12 @@ def handle_users_reply(bot, update):
     else:
         user_state = db.get(chat_id).decode("utf-8")
 
+    start_with_args = partial(start,
+                              moltin_client_id=moltin_client_id,
+                              moltin_client_secret=moltin_client_secret)
+
     states_functions = {
-        'START': start,
+        'START': start_with_args,
         'ECHO': echo
     }
     state_handler = states_functions[user_state]
@@ -94,19 +111,6 @@ def handle_users_reply(bot, update):
         logging.exception(err)
 
 
-def get_database_connection():
-    """
-    Возвращает конекшн с базой данных Redis, либо создаёт новый, если он ещё не создан.
-    """
-    global _database
-    if _database is None:
-        database_password = os.getenv("REDIS_PASSWORD")
-        database_host = os.getenv("REDIS_HOST")
-        database_port = int(os.getenv("REDIS_PORT"))
-        _database = redis.Redis(host=database_host, port=database_port, password=database_password)
-    return _database
-
-
 def main():
     dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     if os.path.exists(dotenv_path):
@@ -115,17 +119,29 @@ def main():
     token = os.getenv("TELEGRAM_TOKEN")
     logger_token = os.getenv("TOKEN_TELEGRAM_LOGGER")
     logger_chat_id = os.getenv("CHAT_ID")
+    moltin_client_id = os.getenv('MOLTIN_CLIENT_ID')
+    moltin_client_secret = os.getenv('MOLTIN_CLIENT_SECRET')
+    database_password = os.getenv("REDIS_PASSWORD")
+    database_host = os.getenv("REDIS_HOST")
+    database_port = int(os.getenv("REDIS_PORT"))
+
+    database = redis.Redis(host=database_host, port=database_port, password=database_password)
 
     logging.basicConfig(level=logging.INFO, format='{asctime} - {levelname} - {name} - {message}', style='{')
     logger.addHandler(BotHandler(logger_token, logger_chat_id))
     logger.info('Начало работы телеграмм бота Интернет магазин')
 
+    handle_users_reply_with_args = partial(handle_users_reply,
+                                           moltin_client_id=moltin_client_id,
+                                           moltin_client_secret=moltin_client_secret,
+                                           db=database)
+
     updater = Updater(token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CallbackQueryHandler(button))
-    dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
-    dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply))
-    dispatcher.add_handler(CommandHandler('start', handle_users_reply))
+    dispatcher.add_handler(CallbackQueryHandler(handle_users_reply_with_args))
+    dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply_with_args))
+    dispatcher.add_handler(CommandHandler('start', handle_users_reply_with_args))
     updater.start_polling()
 
 
