236cfc9d8248d6913b430d60fa32fbf8fed50ce3
pavel <psgaiduk@gmail.com>
2022-03-05 08:47:08 +0300

    add loggers in work moltin

--- work_with_motlin.py
+++ work_with_motlin.py
@@ -14,58 +14,94 @@ class WorkMoltin:
 
     @staticmethod
     def check_status(data):
+        logger.debug(f'get status {data.status_code}')
         if data.status_code >= 300:
             logger.error(f'Что-то пошло не так код {data.status_code}\n{data.text}')
 
     def get_header(self):
+        logger.debug('start get header')
         url = 'https://api.moltin.com/oauth/access_token'
         data = {'client_id': self.client_id, 'client_secret': self.secret_code, 'grant_type': 'client_credentials'}
+        logger.debug(f'data for get token\nurl = {url}\ndata = {data}')
         data_for_token = post(url=url, data=data)
+        logger.debug(f'get data token\n{data_for_token}')
         self.check_status(data_for_token)
         data_for_token_json = data_for_token.json()
+        logger.debug(f'json token\n{data_for_token_json}')
         token = data_for_token_json['access_token']
         header = {'authorization': f'Bearer {token}', 'content-type': 'application/json'}
+        logger.debug(f'return header = {header}')
         return header
 
     def get_products(self, product_id=''):
+        logger.debug(f'Start work get products\nproduct_id = {product_id}')
         url = f'{self.url}products{product_id}'
+        logger.debug(f'create url = {url}')
         products_data = get(url, headers=self.get_header())
         self.check_status(products_data)
-        return products_data.json()['data']
+        products_data_json = products_data.json()
+        logger.debug(f'get products data in json\n{products_data_json}')
+        return products_data_json['data']
 
     def get_image_product(self, product_id):
+        logger.debug(f'Start work get image product\nproduct_id = {product_id}')
         url = f'{self.url}files/{product_id}'
+        logger.debug(f'create url = {url}')
         # Временно заменил, я так понял проблемы с амазоном из-за последних событий
         # image_data = get(url, headers=self.header)
         # self.check_status(image_data)
-        # return image_data.json()['data']['link']['href']
+        # image_data_json = image_data.json()
+        # logger.debug(f'get image data in json\n{image_data_json}')
+        # return image_data_json['data']['link']['href']
         return 'https://e1.edimdoma.ru/data/ingredients/0000/5509/5509-ed4_wide.jpg'
 
     def add_to_cart(self, product_id, chat_id, quantity):
+        logger.debug(f'Start work add to cart\nproduct_id = {product_id}\n'
+                     f'chat_id = {chat_id}\n'
+                     f'quantity = {quantity}')
         url = f'{self.url}carts/{chat_id}/items'
+        logger.debug(f'create url = {url}')
         data = {"data": {"id": product_id, "type": "cart_item", "quantity": quantity}}
+        logger.debug(f'data to post request\n{data}')
         data = post(url, headers=self.get_header(), json=data)
         self.check_status(data)
 
     def get_cart(self, chat_id):
+        logger.debug(f'Start work get cart\nchat_id = {chat_id}')
         url = f'{self.url}carts/{chat_id}'
+        logger.debug(f'create url = {url}')
         cart_price_data = get(url, headers=self.get_header())
         self.check_status(cart_price_data)
-        cart_price = cart_price_data.json()
-        cart_price = cart_price['data']['meta']['display_price']['with_tax']['formatted']
+        cart_price_json = cart_price_data.json()
+        logger.debug(f'get cart price json\n{cart_price_json}')
+        cart_price = cart_price_json['data']['meta']['display_price']['with_tax']['formatted']
+        logger.debug(f'get cart price = {cart_price}')
         url = f'{self.url}carts/{chat_id}/items'
+        logger.debug(f'create url = {url}')
         cart_items_data = get(url, headers=self.get_header())
         self.check_status(cart_items_data)
-        cart_items = cart_items_data.json()
-        return cart_price, cart_items['data']
+        cart_items_json = cart_items_data.json()
+        logger.debug(f'get cart items json\n{cart_items_json}')
+        cart_items = cart_items_json['data']
+        logger.debug(f'cart items = {cart_items}')
+        return cart_price, cart_items
 
     def delete_item_from_cart(self, chat_id, item_id):
+        logger.debug(f'Start work delete item from cart\n'
+                     f'chat_id = {chat_id}\n'
+                     f'item_id = {item_id}')
         url = f'{self.url}carts/{chat_id}/items/{item_id}'
+        logger.debug(f'create url = {url}')
         data = delete(url, headers=self.get_header())
         self.check_status(data)
 
     def create_customer_in_cms(self, chat_id, email, db):
+        logger.debug(f'Start work create customer in cms\n'
+                     f'chat_id = {chat_id}\n'
+                     f'email = {email}\n'
+                     f'db = {db}')
         url = f'{self.url}customers'
+        logger.debug(f'create url = {url}')
         data = {
          "data": {
            "type": "customer",
@@ -74,7 +110,11 @@ class WorkMoltin:
            "password": "mysecretpassword"
          }
         }
+        logger.debug(f'create data for get customer\n{data}')
         customer_data = post(url, headers=self.get_header(), json=data)
         self.check_status(customer_data)
-        customer_id = customer_data.json()['data']['id']
+        customer_id_json = customer_data.json()
+        logger.debug(f'get customer id json\n{customer_id_json}')
+        customer_id = customer_id_json['data']['id']
+        logger.debug(f'customer_id = {customer_id}')
         db.set(f'customer_{chat_id}', customer_id)
